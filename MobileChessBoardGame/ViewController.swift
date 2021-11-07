7d8eadb1a0ab3167963b93856f6bb0f88ac3c746
nikoagge <nikoagge@users.noreply.github.com>
2021-11-07 21:13:03 +0200

    Initial Commit

---
+++ MobileChessBoardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileChessBoardGame
+//
+//  Created by Nikos Aggelidis on 7/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
