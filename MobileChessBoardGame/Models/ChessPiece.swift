f365a7e4a5725cd8552d5f69b4d44206f7bec6c4
nikoagge <nikoagge@users.noreply.github.com>
2021-11-10 22:45:48 +0200

    Adding 12 piece images into Xcode.

---
+++ MobileChessBoardGame/Models/ChessPiece.swift
@@ -0,0 +1,14 @@
+//
+//  ChessPiece.swift
+//  MobileChessBoardGame
+//
+//  Created by Nikos Aggelidis on 10/11/21.
+//
+
+import Foundation
+
+struct ChessPiece: Hashable {
+    let column: Int
+    let row: Int
+    let imageName: String
+}
