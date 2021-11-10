291b25722c1c0b88fdf28c413ba27002e98f2166
nikoagge <nikoagge@users.noreply.github.com>
2021-11-10 20:22:38 +0200

    Drawed chess board for, adjusted for all devices.

--- MobileChessBoardGame/Views/BoardView.swift
+++ MobileChessBoardGame/Views/BoardView.swift
@@ -8,19 +8,29 @@
 import UIKit
 
 class BoardView: UIView {
-    let originX: CGFloat = 24
-    let originY: CGFloat = 38
-    let cellSide: CGFloat = 42
+    let ratio: CGFloat = 0.8
+    var originX: CGFloat = -31
+    var originY: CGFloat = -4
+    var cellSide: CGFloat = -13
     
     override func draw(_ rect: CGRect) {
+        cellSide = bounds.width * ratio / 8
+        originX = bounds.width * (1 - ratio) / 2
+        originY = bounds.height * (1 - ratio) / 2
         drawBoard()
     }
 }
 
 private extension BoardView {
     func drawBoard() {
-        drawSquare(column: 0, row: 0, color: .white)
-        drawSquare(column: 1, row: 0, color: .black)
+        for row in 0..<4 {
+            for column in 0..<4 {
+                drawSquare(column: column * 2, row: row * 2, color: .yellow)
+                drawSquare(column: 1 + column * 2, row: row * 2, color: .black)
+                drawSquare(column: column * 2, row: 1 + row * 2, color: .black)
+                drawSquare(column: 1 + column * 2, row: 1 + row * 2, color: .yellow)
+            }
+        }
     }
     
     func drawSquare(column: Int, row: Int, color: UIColor) {
