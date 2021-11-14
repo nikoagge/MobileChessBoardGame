2feb48da7b550308c2d267dba74351cdb4d57c7d
nikoagge <nikoagge@users.noreply.github.com>
2021-11-14 12:50:10 +0200

    Started to implement MultipeerConnectivity functionality.

--- MobileChessBoardGame/ViewControllers/HomeViewController.swift
+++ MobileChessBoardGame/ViewControllers/HomeViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVFoundation
+import MultipeerConnectivity
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var boardView: BoardView!
@@ -14,11 +15,17 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var resetButton: UIButton!
     
     var chessEngine = ChessEngine()
+    
     var audioPlayer: AVAudioPlayer!
+    
+    var peerID: MCPeerID!
+    var session: MCSession!
+    var nearbyServiceAdvertiser: MCNearbyServiceAdvertiser!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        peerConfiguration()
         audioPlayerConfiguration()
         chessEngine.initializeGame()
         boardViewConfiguration()
@@ -46,7 +53,60 @@ extension HomeViewController: ChessDelegate {
     }
 }
 
+extension HomeViewController: MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .notConnected:
+            debugPrint("Not connected: \(peerID.displayName)")
+            
+        case .connecting:
+            debugPrint("Connecting: \(peerID.displayName)")
+            
+        case .connected:
+            debugPrint("Connected: \(peerID.displayName)")
+            
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        debugPrint("Received data: \(peerID.displayName)")
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+}
+
+extension HomeViewController: MCBrowserViewControllerDelegate {
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+}
+
+extension HomeViewController: MCNearbyServiceAdvertiserDelegate {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        invitationHandler(true, session)
+    }
+}
+
 private extension HomeViewController {
+    func peerConfiguration() {
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        session.delegate = self
+    }
+    
     func audioPlayerConfiguration() {
         let url = Bundle.main.url(forResource: "chess-move", withExtension: "wav")!
         audioPlayer = try? AVAudioPlayer(contentsOf: url)
