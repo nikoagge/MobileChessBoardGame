ff4c6f019dbf214279a2feb8df2734be7e874ab0
nikoagge <nikoagge@users.noreply.github.com>
2021-11-14 10:17:34 +0200

    Refactoring setup of chess pieces on ChessEngine.

--- MobileChessBoardGame/Models/ChessEngine.swift
+++ MobileChessBoardGame/Models/ChessEngine.swift
@@ -32,37 +32,23 @@ struct ChessEngine {
     
     mutating func initializeGame() {
         pieces.removeAll()
-        pieces.insert(ChessPiece(column: 0, row: 0, imageName: "Rook-black"))
-        pieces.insert(ChessPiece(column: 7, row: 0, imageName: "Rook-black"))
-        pieces.insert(ChessPiece(column: 0, row: 7, imageName: "Rook-white"))
-        pieces.insert(ChessPiece(column: 7, row: 7, imageName: "Rook-white"))
-        pieces.insert(ChessPiece(column: 1, row: 0, imageName: "Knight-black"))
-        pieces.insert(ChessPiece(column: 6, row: 0, imageName: "Knight-black"))
-        pieces.insert(ChessPiece(column: 1, row: 7, imageName: "Knight-white"))
-        pieces.insert(ChessPiece(column: 6, row: 7, imageName: "Knight-white"))
-        pieces.insert(ChessPiece(column: 2, row: 0, imageName: "Bishop-black"))
-        pieces.insert(ChessPiece(column: 5, row: 0, imageName: "Bishop-black"))
-        pieces.insert(ChessPiece(column: 2, row: 7, imageName: "Bishop-white"))
-        pieces.insert(ChessPiece(column: 5, row: 7, imageName: "Bishop-white"))
+        for row in 0..<2 {
+            pieces.insert(ChessPiece(column: row * 7, row: 0, imageName: "Rook-black"))
+            pieces.insert(ChessPiece(column: row * 7, row: 7, imageName: "Rook-white"))
+            pieces.insert(ChessPiece(column: 1 + row * 5, row: 0, imageName: "Knight-black"))
+            pieces.insert(ChessPiece(column: 1 + row * 5, row: 7, imageName: "Knight-white"))
+            pieces.insert(ChessPiece(column: 2 + row * 3, row: 0, imageName: "Bishop-black"))
+            pieces.insert(ChessPiece(column: 2 + row * 3, row: 7, imageName: "Bishop-white"))
+        }
+        
         pieces.insert(ChessPiece(column: 3, row: 0, imageName: "Queen-black"))
         pieces.insert(ChessPiece(column: 3, row: 7, imageName: "Queen-white"))
         pieces.insert(ChessPiece(column: 4, row: 0, imageName: "King-black"))
         pieces.insert(ChessPiece(column: 4, row: 7, imageName: "King-white"))
-        pieces.insert(ChessPiece(column: 0, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 1, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 2, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 3, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 4, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 5, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 6, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 7, row: 1, imageName: "Pawn-black"))
-        pieces.insert(ChessPiece(column: 0, row: 6, imageName: "Pawn-white"))
-        pieces.insert(ChessPiece(column: 1, row: 6, imageName: "Pawn-white"))
-        pieces.insert(ChessPiece(column: 2, row: 6, imageName: "Pawn-white"))
-        pieces.insert(ChessPiece(column: 3, row: 6, imageName: "Pawn-white"))
-        pieces.insert(ChessPiece(column: 4, row: 6, imageName: "Pawn-white"))
-        pieces.insert(ChessPiece(column: 5, row: 6, imageName: "Pawn-white"))
-        pieces.insert(ChessPiece(column: 6, row: 6, imageName: "Pawn-white"))
-        pieces.insert(ChessPiece(column: 7, row: 6, imageName: "Pawn-white"))
+        
+        for column in 0..<8 {
+            pieces.insert(ChessPiece(column: column, row: 1, imageName: "Pawn-black"))
+            pieces.insert(ChessPiece(column: column, row: 6, imageName: "Pawn-white"))
+        }
     }
 }
